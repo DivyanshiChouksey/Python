e31ba8ffd350c3f3adab865078a719e7547238d7
DivyanshiChouksey <89761026+DivyanshiChouksey@users.noreply.github.com>
2021-11-24 23:42:08 +0530

    Update prime no. by using function.py

--- prime no. by using function.py
+++ prime no. by using function.py
@@ -1,11 +1,8 @@
-a=int(input("Enter a number "))
+a  = int(input("Enter a number "))
 def isprime(a):
-	for i in range(2,a) :
-			if a%i==0 :
-				print("not a prime no.")
-				break	
-			elif 1 + i == a :
-					print("prime number")
-
-isprime(a)
+	for i in range(2,int(a**0.5)+1):
+			if a % i == 0 :
+				return "this number is not a prime number"
+			return "this number is a prime number"
+print(isprime(a))
     
