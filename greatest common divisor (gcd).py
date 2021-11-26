c9db477d02633e8b376fd9a9e226ae44bbdb9ce5
DivyanshiChouksey <89761026+DivyanshiChouksey@users.noreply.github.com>
2021-11-26 23:52:01 +0530

    Update greatest common divisor (gcd).py

--- greatest common divisor (gcd).py
+++ greatest common divisor (gcd).py
@@ -1,8 +1,21 @@
-# greatest common divisor (gcd)
+# Greatest Common Divisor
+# O(log(mn)) Time complexity | O()Space complexity
 a = 3918848
-b = 1653264
-while a%b !=0:
+b = 1653264  # smallest always
+while a%b != 0: 
      mod = a%b
      a = b
      b = mod
 print(b)
+
+
+# O(m+n) Time Complexity | O(1) Space complexity
+a = 98
+b = 56
+gcd = 1
+for d in range(1,a+b) :
+     if a%d == 0 and b%d == 0 :
+          gcd = max(gcd,d)
+print("The GCD of",a,"and",b,"is",gcd)
+
+
